ea35182b1e16e6b7f7ed43caf30eedca42cc511a
Kiko <mmhabib11@gmail.com>
2014-11-22 11:40:07 -0500

    Logic for new game button

--- script.js
+++ script.js
@@ -3,43 +3,45 @@ var $o = [];
 var $x = [];
 
 $(document).ready(function() {
-	
+	$(document).on('click', 'button', function() {
+		location.reload();
+	});
+	start();
+});
 
+var start = function() {
 	$('td').click(function(){
 		if( $turn % 2 === 0 ) {
 			$(this).text("X");
-			var $clicked = $(this).attr("id");
+			var $boxId = $(this).attr("id");
 			$(this).off('click');
-			$x.push(parseInt($clicked));
+			$x.push(parseInt($boxId));
 			$('#player').text("Player one's turn");
 			console.log($x);
 			winner();
 		}
 		else {
 			$(this).text("O");
-			var $clicked = $(this).attr("id");
+			var $boxId = $(this).attr("id");
 			$(this).off('click');
-			$o.push(parseInt($clicked));
+			$o.push(parseInt($boxId));
 			$('#player').text("Player two's turn");
 			console.log($o);
 			winner();
-			
 		}
 		$turn++;
 	});
-
-	
-});
+}
 
 var winner = function() {
 	var $sumX = 0;
 	var $sumO = 0;
 	
 	if ($x.length >= 3) {
-		for (i = 0; i < 3; i++) {
+		for (i = 0; i < $x.length; i++) {
 			$sumX += $x[i];
 		}
-		if ($sumX === 15) {
+		if ( ($x[0]) + ($x[1]) + ($x[2]) === 15 ) {
 			alert("Player 1 is a winner (first 3)");
 		}else if ( ($x[0]) + ($x[1]) + ($x[3]) === 15 ) {
 			alert("Player 1 is a winner (0,1,3)");
@@ -53,23 +55,23 @@ var winner = function() {
 			alert("Player 1 is a winner (1,2,4)");
 		}else if ( ($x[2]) + ($x[3]) + ($x[4]) === 15 ) {
 			alert("Player 1 is a winner (2,3,4)");
+		}else if ( ($x[0]) + ($x[2]) + ($x[3]) === 15 ) {
+			alert("Player 1 is a winner (0,2,3)");
 		}
 	}
 
 	if ($o.length >= 3) {
-		for (i = 0; i < 3; i++) {
+		for (i = 0; i < $o.length; i++) {
 			$sumO += $o[i];
 		}
-		if ($sumO === 15) {
-			alert("Plaoer 2 is a winner (first 3)");
-		}else if ( ($o[0]) + ($o[1]) + ($o[2]) === 15 ) {
-			alert("Plaoer 2 is a winner (0,1,2)");
+		if ( ($o[0]) + ($o[1]) + ($o[2]) === 15 ) {
+			alert("Player 2 is a winner (first 3)");
 		}else if ( ($o[0]) + ($o[1]) + ($o[3]) === 15 ) {
-			alert("Plaoer 2 is a winner (0,1,4)");
+			alert("Player 2 is a winner (0,1,2)");
 		}else if ( ($o[0]) + ($o[2]) + ($o[3]) === 15 ) {
-			alert("Plaoer 2 is a winner (0,2,4)");
+			alert("Player 2 is a winner (0,1,4)");
 		}else if ( ($o[1]) + ($o[2]) + ($o[3]) === 15 ) {
-			alert("Player 2 is a winner (1,2,3)");
+			alert("Player 2 is a winner (0,2,4)");
 		}
 	}
 }
